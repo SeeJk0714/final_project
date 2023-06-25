46dacafb2e8c629f61638fbd001ddc5ab95a0596
jiekai <jiekai22s@forwardschool.edu.my>
2023-06-25 21:45:03 +0800

    last update

--- Final_Project (1).sql
+++ Final_Project (1).sql
@@ -28,12 +28,17 @@ CREATE TABLE `carts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `carts` (`id`, `quantity`, `user_id`, `product_id`, `order_id`, `editor_by`, `create_at`) VALUES
-(80,	1,	12,	17,	58,	17,	'2023-06-22 02:17:57'),
-(86,	2,	12,	17,	60,	17,	'2023-06-22 02:33:42'),
-(89,	3,	16,	17,	NULL,	17,	'2023-06-22 02:49:18'),
 (91,	3,	15,	19,	62,	17,	'2023-06-22 02:56:56'),
 (93,	2,	12,	15,	63,	11,	'2023-06-22 03:56:01'),
-(94,	4,	12,	14,	NULL,	11,	'2023-06-22 03:56:15');
+(95,	2,	15,	12,	64,	7,	'2023-06-22 05:46:02'),
+(96,	1,	15,	13,	64,	7,	'2023-06-22 05:46:02'),
+(97,	5,	25,	19,	65,	17,	'2023-06-22 06:18:26'),
+(98,	1,	25,	19,	66,	17,	'2023-06-22 06:19:25'),
+(99,	1,	25,	15,	66,	11,	'2023-06-22 06:19:25'),
+(102,	5,	16,	19,	67,	17,	'2023-06-22 06:56:22'),
+(103,	3,	16,	14,	67,	11,	'2023-06-22 06:56:22'),
+(104,	1,	12,	21,	68,	17,	'2023-06-22 07:03:17'),
+(105,	1,	12,	21,	68,	17,	'2023-06-22 07:03:17');
 
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments` (
@@ -50,23 +55,16 @@ CREATE TABLE `comments` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `comments` (`id`, `comment`, `create_at`, `user_id`, `product_id`) VALUES
-(16,	'qweeeeeeeee',	'2023-06-09 13:52:08',	5,	15),
-(17,	'qqqqqqqqqqqqqqqqqqq',	'2023-06-09 13:52:15',	5,	15),
 (30,	'dddddddddddddddddddddddddd',	'2023-06-17 15:18:54',	16,	14),
-(32,	'assssssssssss',	'2023-06-19 01:10:43',	12,	17),
-(35,	'rrrr',	'2023-06-20 03:20:10',	13,	17),
-(36,	'jkl;lkkl;',	'2023-06-20 03:29:32',	13,	17),
-(38,	'csdadadadad\r\n',	'2023-06-20 06:59:16',	12,	17),
-(40,	'qqqqqqqqqqqqqqqqqqq',	'2023-06-21 02:56:44',	15,	17),
-(41,	'This nice or not?',	'2023-06-21 07:53:08',	23,	17),
-(42,	'V2 really Pro???',	'2023-06-21 07:53:27',	23,	17),
 (44,	'ABC',	'2023-06-21 07:58:49',	23,	12),
 (47,	'got stock now?',	'2023-06-21 08:09:53',	13,	12),
 (48,	' விசைகளை உடைப்பது எளிது!',	'2023-06-21 08:11:22',	15,	12),
 (50,	'இது மலிவானது மற்றும் பயன்படுத்த எளிதானது',	'2023-06-21 08:14:19',	15,	14),
 (51,	'மோசடி',	'2023-06-21 08:15:33',	15,	14),
 (52,	'いいですね',	'2023-06-21 08:16:09',	15,	15),
-(57,	'hhkjhfjkhfjskdhfk\r\n',	'2023-06-22 02:56:47',	15,	19);
+(57,	'hhkjhfjkhfjskdhfk\r\n',	'2023-06-22 02:56:47',	15,	19),
+(59,	'sdfghjkl',	'2023-06-22 06:10:30',	12,	13),
+(60,	'sdfghj',	'2023-06-22 06:10:46',	12,	11);
 
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE `orders` (
@@ -80,10 +78,13 @@ CREATE TABLE `orders` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `orders` (`id`, `total_amount`, `user_id`, `create_at`) VALUES
-(58,	749,	12,	'2023-06-22 02:17:57'),
-(60,	1588,	12,	'2023-06-22 02:33:42'),
 (62,	270,	15,	'2023-06-22 02:56:56'),
-(63,	310,	12,	'2023-06-22 03:56:01');
+(63,	310,	12,	'2023-06-22 03:56:01'),
+(64,	700,	15,	'2023-06-22 05:46:02'),
+(65,	450,	25,	'2023-06-22 06:18:26'),
+(66,	245,	25,	'2023-06-22 06:19:25'),
+(67,	1047,	16,	'2023-06-22 06:56:22'),
+(68,	600,	12,	'2023-06-22 07:03:17');
 
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products` (
@@ -109,8 +110,8 @@ INSERT INTO `products` (`id`, `title`, `price`, `image_url`, `status`, `create_a
 (13,	'G102 LIGHTSYNC RGB Gaming Mouse',	100,	'https://www.techlandbd.com/image/catalog/Mouse/Logitech/G102/logitech-g102-1.jpg',	'publish',	'2023-06-01 16:00:23',	'2023-06-17 07:26:59',	7,	7),
 (14,	'RAZER DEATHADDER V2 X HYPERSPEED',	199,	'https://cdn.shopify.com/s/files/1/1974/9033/products/1_fb147bbd-2e0b-48a1-ac3a-668ee7f4a6e3_1024x1024.jpg?v=1636709804',	'publish',	'2023-06-06 08:14:25',	'2023-06-17 07:27:04',	11,	11),
 (15,	'RAZER BASILISK X HYPERSPEED',	155,	'https://cdn.shopify.com/s/files/1/1974/9033/products/1_71af9a70-0a2e-443e-8551-f607357c4591_1024x1024.jpg?v=1594635685',	'publish',	'2023-06-06 08:18:34',	'2023-06-17 07:27:13',	11,	11),
-(17,	'RAZER VIPER V2 PRO',	749,	'https://www.dateks.lv/images/pic/2400/2400/964/781.jpg',	'publish',	'2023-06-17 15:28:41',	'2023-06-21 08:40:48',	5,	17),
-(19,	'RAZER VIPER MINI',	90,	'https://www.justlaptops.co.nz/image/cache/data/viper-mini-25805-1200x1200.jpg',	'publish',	'2023-06-22 02:56:21',	'2023-06-22 02:56:26',	17,	17);
+(19,	'RAZER VIPER MINI',	90,	'https://www.justlaptops.co.nz/image/cache/data/viper-mini-25805-1200x1200.jpg',	'publish',	'2023-06-22 02:56:21',	'2023-06-22 02:56:26',	17,	17),
+(21,	'RAZER OROCHI V2',	300,	'https://www.startech.com.bd/image/cache/catalog/mouse/razer/orochi-v2/orochi-v2-black-01-500x500.jpg',	'publish',	'2023-06-22 06:57:50',	'2023-06-22 06:58:04',	17,	17);
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -124,7 +125,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `create_at`) VALUES
-(5,	'rex',	'rex@gmail.com',	'$2y$10$yU9TkA3AOuOxLYIDK5jj.uUZj60qzyvVGH8NeX1O2uOEdjgn9cKn.',	'admin',	'2023-06-21 08:07:37'),
 (7,	'editor2',	'editor2@gmail.com',	'$2y$10$bQAR3WneGGJk56th1/gKZuGD4s2l8sEN47ZYqn70x9Jgd.oRvB4g.',	'editor',	'2023-06-15 04:08:42'),
 (11,	'editor',	'editor@gmail.com',	'$2y$10$b9xMePmixdGZSKgoZyvIYui02gnoFzTvSAwEMgslAzfr0smVCh3dq',	'editor',	'2023-06-12 08:01:56'),
 (12,	'user',	'user@gmail.com',	'$2y$10$EqpiqrE.UC1FnEIM2jgSkOU6sK8XvShepA1/PtcW57aIkbNHVucsG',	'user',	'2023-06-13 03:34:52'),
@@ -135,6 +135,8 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `create_at`) VAL
 (17,	'editor3',	'editor3@gmail.com',	'$2y$10$QbVhmnDlybdLBHPckddRne37b3/1JGOkCPmBk6R1lsWumW2FKKE/m',	'editor',	'2023-06-17 15:25:18'),
 (18,	'John',	'john@gmail.com',	'$2y$10$m5YnCRX/jJ3Ulh9eqljyk.c0Eh9N/EZJ/nq2bX/DNNv5srQVOFBhm',	'user',	'2023-06-19 01:07:45'),
 (23,	'denish',	'denish@gmail.com',	'$2y$10$QhQUQLsqCgfjdpmx7Ko/4erFkzTcdcNBtXR//UQEh9CDjgSlL4wvm',	'user',	'2023-06-21 07:49:57'),
-(24,	'JIe Kai See',	'jiekai0714@gmail.com',	'$2y$10$o6zFvQ5unTsKHZphjOejQeeGobhLP8x0lzeoKK/UruPUP8gROYPw6',	'editor',	'2023-06-21 08:08:36');
+(24,	'JIe Kai See',	'jiekai0714@gmail.com',	'$2y$10$o6zFvQ5unTsKHZphjOejQeeGobhLP8x0lzeoKK/UruPUP8gROYPw6',	'editor',	'2023-06-21 08:08:36'),
+(25,	'alvinlee',	'alvinlee@gmail.com',	'$2y$10$FEY37ymYOHJb/6qm.TJZnOR7VZvcCjRYTX5yczvKJ3ID6xZMhi/US',	'user',	'2023-06-22 06:17:13'),
+(26,	'useer5',	'user5@gmail.com',	'$2y$10$/Mqmpt.eJEz2tiyR3Lb7QutDUL3.bGfWmbiQAvgBBjcdXXd2d94u.',	'user',	'2023-06-22 07:00:57');
 
--- 2023-06-22 04:00:14
+-- 2023-06-25 13:42:15
